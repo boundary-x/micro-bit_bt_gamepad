7ef293096b0b71b061488fade0b5bf2be15d0f9a
boundary-x <hi@boundaryx.io>
2026-09-12 16:54:49 +0900

    Rename header help button to Support and refresh cached page

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const cacheName = 'gamepad-support-v1';
+const cacheName = 'gamepad-support-v2';
 const precacheResources = [
     'index.html',
     // CSS
@@ -41,7 +41,7 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('activate', event => {
-    event.waitUntil(caches.delete('cache-v1'));
+    event.waitUntil(Promise.all([caches.delete('cache-v1'), caches.delete('gamepad-support-v1')]));
 });
 
 self.addEventListener('fetch', event => {
